c0c862ceb250cfdf67f67b8d97c19e054784d3cb
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-12-28 01:42:45 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,7 @@ const HomePage = () => {
   return (
     <Container sx={{ mt: { xs: '5px', sm: '20px' } }}>
       <Typography component="h1" variant="h2" sx={{ textAlign: 'center' }}>
-        Hello! Welcome to the Phonebook. This app helps you to save and find
-        your contacts in easy, fast, and comfortable ways.
+        Hello! Welcome to the Phonebook.
       </Typography>
     </Container>
   );
